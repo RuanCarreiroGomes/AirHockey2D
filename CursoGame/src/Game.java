c87b36a4a8bcb3fa140221511be3987e4d5e2c9d
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2021-12-05 12:34:25 -0300

    Cap-02

--- CursoGame/src/Game.java
+++ CursoGame/src/Game.java
@@ -5,10 +5,56 @@ import java.awt.Graphics;
 // Janela em que o jogo ocorrerá de fato:
 
 public class Game extends JPanel{
+	private Bola bola;
+	
 	
 	public Game() {
+		bola = new Bola();
 		setFocusable(true);
 		setLayout(null);
+		
+		/** O que são "Threads"?
+		É um recurso da linguagem java que permite à aplicação tire vantagens
+		dos modernos processadores com diversos núcleos de processamento. Em nosso game,
+		o uso de Threads se faz necessário pricipalmente pelo recurso de pausa de execução
+		que ele permite (Thread.sleep).
+		*/
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				gameloop();
+			}
+		}).start(); // => start() => run() => [ gameloop() => handlerEvents() => update() => render() => pausa] 
+	}
+	
+	// GAMELOOP ------------------------
+	// Animação do objeto
+	public void gameloop(){
+		
+		while (true) {
+			
+			handlerEvents();
+			update();
+			render();
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				
+			}
+		}
+	}
+	
+	public void handlerEvents(){
+		
+	}
+	
+	public void update(){
+		bola.posX = bola.posX + bola.velX; // velocidade do objeto
+	}
+	
+	public void render(){
+		repaint(); // Redesenha a tela a cada repetição do gameloop()
 	}
 	
 	// MÉTODO SOBESCRITO ---------------
@@ -17,6 +63,8 @@ public class Game extends JPanel{
 		super.paintComponent(g);
 		setBackground(Color.LIGHT_GRAY);
 		g.setColor(Color.red);
-		g.fillOval(100, 100, 50, 50);
+		
+		// Posição e dimensão do obj "Bola" com parâmetros relativos ao obj
+		g.fillOval(bola.posX, bola.posY, bola.raio * 2, bola.raio * 2);
 	}
 }
